dc4b39fc9e7f881e91ee72716b72342fca71a8f3
Adam Lin <ycl2112@columbia.edu>
2021-04-09 12:16:17 -0400

    edit api

--- apigClient.js
+++ apigClient.js
@@ -53,7 +53,7 @@ apigClientFactory.newClient = function (config) {
 
     
     // extract endpoint and path from url
-    var invokeUrl = 'https://idxj4tbwke.execute-api.us-east-1.amazonaws.com/Stage';
+    var invokeUrl = 'https://9mt39hc2nl.execute-api.us-east-1.amazonaws.com/Stage';
     var endpoint = /(^https?:\/\/[^\/]+)/g.exec(invokeUrl)[1];
     var pathComponent = invokeUrl.substring(endpoint.length);
 
